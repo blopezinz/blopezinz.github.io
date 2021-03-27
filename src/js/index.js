71166c6e509c63bb55603e6cd87e457e7dd8ed31
blopezinz <b.lopezinzulza@gmail.com>
2021-03-27 13:44:47 -0300

    update dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-const nav = document.getElementById('nav');
+let title = document.getElementsByClassName('content--title');
 
-window.addEventListener('scroll', function(e) {
+
+window.addEventListener('scroll', function() {
+    const nav = document.getElementById('nav');
     let positionPage = window.pageYOffset;
     if(positionPage !== 0)
     {
@@ -10,19 +12,27 @@ window.addEventListener('scroll', function(e) {
     }
 });
 
-let dark = document.getElementById("dark");
-let light = document.getElementById("light");
-
-
+changeTitle("#6a00f4");
 
 function changeDark(mode) {
+    const dark = document.getElementById("dark");
+    const light = document.getElementById("light");
+    
     if(mode === 0){
         dark.className = "nav--item dark-mode__disabled";
         light.className = "nav--item";
-        document.body.style.backgroundColor = "#303030";
+        document.body.style.backgroundColor = "#424242";
+        changeTitle("#fff");
     }else if(mode === 1 ){
         light.className = "nav--item dark-mode__disabled";
         dark.className = "nav--item";
         document.body.style.backgroundColor = "#fff";
+        changeTitle("#6a00f4");
+    }
+}
+
+function changeTitle(color){
+    for(i = 0; title.length ; i++) {
+        title[i].style.color = color;
     }
 }
